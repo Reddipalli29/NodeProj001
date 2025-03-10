da5688f6184ce9381c3e5267d49af62f76eee4f0
Reddipalli29 <naresh.reddipa@gmail.com>
2025-03-10 02:11:16 -0700

    initial project

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+
+const port = 3000
+
+app.get('/',(req,res)=>
+    {
+        res.send(`Hello World! ${port}`)
+    
+    })
+
+
+app.listen(port,()=>{
+    console.log(`Example app listerning on port ${port}`)
+}
+)
+
